dde8a4f653df983b32ddb7a5e7f5ce228dbf5fd1
ruyar4 <ruyar410@gmail.com>
2015-01-16 12:42:16 -0300

    ahora tiene jquery

--- js/dj.js
+++ js/dj.js
@@ -16,18 +16,39 @@ function loadSound(url) {
   request.send();
 }
 
+var audio1 = document.getElementById("audio1");
+var audio2 = document.getElementById("audio2");
+
 function play1(){
 	document.getElementById("audio1").play();
+	return true;
 }
-
 function stop1(){
 	document.getElementById("audio1").pause();
+	return false;
+}
+function loop1(){
+	document.getElementById("audio1").loop();
 }
 
 function play2(){
 	document.getElementById("audio2").play();
+	return true;
 }
-
 function stop2(){
 	document.getElementById("audio2").pause();
+	return false;
+}
+function loop2(){
+	document.getElementById("audio2").loop();
+}
+
+function playStopAll(){
+	if(play1()==true || play2()==true){
+		stop1();
+		stop2();
+	}else{
+		play1();
+		play2();
+	}
 }
