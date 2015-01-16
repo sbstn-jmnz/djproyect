5ecac5f46a8457a80f00193d0d3dd55d5abfa748
Christian Helbig <christianhelbig@MacBook-Pro-de-CHELBIG.local>
2015-01-16 13:17:31 -0300

    animacion con canvas

--- js/dj.js
+++ js/dj.js
@@ -31,3 +31,23 @@ function play2(){
 function stop2(){
 	document.getElementById("audio2").pause();
 }
+
+var canvas = document.getElementById('myCanvas');    
+    var context = canvas.getContext('2d');
+    var counter = 0
+
+    function step()
+    {
+        counter++;
+        for (var i = 0; i < 300; i++){
+          for (var j = 0; j < 300; j++)  {
+                if ((i + j + counter) % 2 == 0) {
+                    context.fillStyle = '#1C61FD';
+                } else {
+                    context.fillStyle = '#DE2AFD';
+                }
+                context.fillRect(i * 50, j * 50, 49, 49); // X, Y, 
+        }
+      }
+    }
+    intervalHandler = setInterval(step, 600);
