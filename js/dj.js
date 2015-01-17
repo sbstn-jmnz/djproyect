d105eb59ff89929d372223c4cb15e1a73e5ea914
ruyar4 <ruyar410@gmail.com>
2015-01-17 00:22:33 -0300

    boton play/stop maestro listo

--- js/dj.js
+++ js/dj.js
@@ -16,29 +16,43 @@ function loadSound(url) {
   request.send();
 }
 
+var numero = false;
 var audio1 = document.getElementById("audio1");
 var audio2 = document.getElementById("audio2");
 
-function play1(){
-	document.getElementById("audio1").play();
+function play1(valor){
+  if(valor==false){
+	 document.getElementById("audio1").play();
+   valor = true;
+  }
 	//var html = "";
 	//$(".progress-bar").css("width","10%")
-	return true;
+	return valor;
 }
-function stop1(){
-	document.getElementById("audio1").pause();
-	return false;
+function stop1(valor){
+  if(valor==true){
+	 document.getElementById("audio1").pause();
+   valor = false;
+  }
+	return valor;
 }
 function loop1(){
 	document.getElementById("audio1").loop();
 }
 
-function play2(){
-	document.getElementById("audio2").play();
-	return true;
+function play2(valor){
+	if(valor==false){
+   document.getElementById("audio2").play();
+   valor = true;
+  }
+	return valor;
 }
-function stop2(){
-	document.getElementById("audio2").pause();
+function stop2(valor){
+	if(valor==true){
+   document.getElementById("audio2").pause();
+   valor = false;
+  }
+  return valor;
 }
 
 
@@ -69,23 +83,60 @@ function loop2(){
 }
 
 function playStopAll(){
-	if(play1()==true || play2()==true){
-		stop1();
-		stop2();
-	}else{
-		play1();
-		play2();
-	}
+	if(numero==true){
+    stop1(true);
+    stop2(true);
+    numero = false;
+  }else{
+    play1(false);
+    play2(false);
+    numero = true;
+  }
 }
-
+////////////Slider/////////////////
 $(function() {  
-    $( "#slider" ).slider();  
+  
+    var slider = $('#slider'),  
+        tooltip = $('.tooltip');  
+  
+    tooltip.hide();  
+  
+    slider.slider({  
+        range: "min",  
+        min: 1,  
+        value: 35,  
+  
+        start: function(event,ui) {  
+          tooltip.fadeIn('fast');  
+        },  
+  
+        slide: function(event, ui) {  
+  
+            var value = slider.slider('value'),  
+                volume = $('.volume'); 
+
+
+
+            tooltip.css('left', value).text(ui.value);  
+  
+            if(value <= 5) {   
+                volume.css('background-position', '0 0');  
+            }   
+            else if (value <= 25) {  
+                volume.css('background-position', '0 -25px');  
+            }   
+            else if (value <= 75) {  
+                volume.css('background-position', '0 -50px');  
+            }   
+            else {  
+                volume.css('background-position', '0 -75px');  
+            };  
+  
+        },  
+  
+        stop: function(event,ui) {  
+          tooltip.fadeOut('fast');  
+        },  
+    });  
+  
 });  
-
-var slider = $('#slider');  
-
-slider.slider({  
-    range: "min",  
-    value: 35,  
-});  
-
