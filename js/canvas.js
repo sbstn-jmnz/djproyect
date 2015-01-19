ef24fbd6c6c575b326728a0d42e9400f2f70db5b
Rudyard Fuster <ruyar410@gmail.com>
2015-01-19 00:18:44 -0300

    mina empezando a bailar

--- js/canvas.js
+++ js/canvas.js
@@ -1,19 +1,21 @@
 var canvas = document.getElementById('myCanvas');    
-                var context = canvas.getContext('2d');
-                var counter = 0
+var context = canvas.getContext('2d');
+var counter = 0
 
-                function step()
-                {
-                  counter++;
-                  for (var i = 0; i < 300; i++){
-                    for (var j = 0; j < 300; j++)  {
-                      if ((i + j + counter) % 2 == 0) {
-                        context.fillStyle = '#1C61FD';
-                      } else {
-                        context.fillStyle = '#DE2AFD';
-                      }
-                                    context.fillRect(i * 50, j * 50, 49, 49); // X, Y, 
-                                  }
-                                }
-                              }
-                              intervalHandler = setInterval(step, 600);
+function step()
+{
+    counter++;
+    for (var i = 0; i < 300; i++){
+        for (var j = 0; j < 300; j++)  {
+            if ((i + j + counter) % 2 == 0) {
+                context.fillStyle = '#1C61FD';
+            } else {
+                context.fillStyle = '#DE2AFD';
+            }
+            context.fillRect(i * 50, j * 50, 49, 49); // X, Y, 
+        }
+     }
+}
+intervalHandler = setInterval(step, 600);
+
+
